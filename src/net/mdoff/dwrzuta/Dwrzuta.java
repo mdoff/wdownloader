ccfd3bf17fd1f230f99e62375dbcabac28388147
mdoff <mdoff@mdoff.net>
2012-08-03 16:20:47 +0200

    init

---
+++ src/net/mdoff/dwrzuta/Dwrzuta.java
@@ -0,0 +1,11 @@
+package net.mdoff.dwrzuta;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: mdoff
+ * Date: 03.08.12
+ * Time: 16:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dwrzuta {
+}
